36352e96f28da22d1d74c9d1dfb2b5dbd1315c0d
IronBoz <106668997+IronBoz@users.noreply.github.com>
2020-09-05 12:59:48 +0000

    Nightly version

--- MyCoin.cs
+++ MyCoin.cs
@@ -17,4 +17,4 @@ contract Token {
 
     /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
     /// @param _from The address of the sender
-    /// @param _to The address of the
+    /// @param _to The address of the recipien
