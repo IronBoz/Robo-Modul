48e2b99d19c47ecf338fac1c1351122578fd30c8
IronBoz <106668997+IronBoz@users.noreply.github.com>
2020-10-17 12:20:22 +0000

    yayin

--- MyCoin.cs
+++ MyCoin.cs
@@ -22,4 +22,4 @@ contract Token {
     /// @return Whether the transfer was successful or not
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
 
-    /// @notice `msg.sender` app
+    /// @notice `msg.sender` approves `_addr
