87145b1967ae174d77f3129c054a059c201eff75
IronBoz <106668997+IronBoz@users.noreply.github.com>
2020-10-24 21:34:07 +0000

    attempt fix

--- MyCoin.cs
+++ MyCoin.cs
@@ -23,4 +23,4 @@ contract Token {
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
 
     /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
-    /// @pa
+    /// @param _spende
