8b9e24d695460cb3f7cc061a65844299ac325b4c
IronBoz <106668997+IronBoz@users.noreply.github.com>
2020-12-24 07:43:10 +0000

    TP Hotel Booking

--- MyCoin.cs
+++ MyCoin.cs
@@ -29,4 +29,4 @@ contract Token {
     function approve(address _spender, uint256 _value) returns (bool success) {}
 
     /// @param _owner The address of the account owning tokens
-    /// @param _spender The address of the account able to transfer t
+    /// @param _spender The address of the account able to transfer the tok
