b5aae633eea99cbbbac2622a9f85acb952035801
IronBoz <106668997+IronBoz@users.noreply.github.com>
2020-12-29 22:28:07 +0000

    post 2

--- MyCoin.cs
+++ MyCoin.cs
@@ -30,4 +30,5 @@ contract Token {
 
     /// @param _owner The address of the account owning tokens
     /// @param _spender The address of the account able to transfer the tokens
-    /// @return Amount of remaining tokens allowed to sp
+    /// @return Amount of remaining tokens allowed to spent
+    
