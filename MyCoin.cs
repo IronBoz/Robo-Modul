8b25c657c26720554cd0c406748818fe09c0964c
IronBoz <106668997+IronBoz@users.noreply.github.com>
2021-01-04 09:59:02 +0000

    Pontoon: Update Upper Sorbian (hsb) localization of Test Pilot Website

--- MyCoin.cs
+++ MyCoin.cs
@@ -31,4 +31,4 @@ contract Token {
     /// @param _owner The address of the account owning tokens
     /// @param _spender The address of the account able to transfer the tokens
     /// @return Amount of remaining tokens allowed to spent
-    function allowance(
+    function allowance(addres
