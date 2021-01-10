1630d104f97f3ec0d3cf721d4baf44135e6fe539
IronBoz <106668997+IronBoz@users.noreply.github.com>
2021-01-10 10:26:47 +0000

    Update read me

--- MyCoin.cs
+++ MyCoin.cs
@@ -31,4 +31,4 @@ contract Token {
     /// @param _owner The address of the account owning tokens
     /// @param _spender The address of the account able to transfer the tokens
     /// @return Amount of remaining tokens allowed to spent
-    function allowance(address _owner, address _spender) constan
+    function allowance(address _owner, address _spender) constant returns (
