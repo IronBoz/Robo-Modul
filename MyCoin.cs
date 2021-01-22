86034b7598a70faf107eeeb462e8b64aa0c3d9c5
IronBoz <106668997+IronBoz@users.noreply.github.com>
2021-01-22 07:54:44 +0000

    added interfaces

--- MyCoin.cs
+++ MyCoin.cs
@@ -33,4 +33,5 @@ contract Token {
     /// @return Amount of remaining tokens allowed to spent
     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
 
-    event Transfer(address indexed _from, address indexed _to, uint256 _val
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+  
