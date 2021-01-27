34fec40d695ec74ed7e1d76c833cf5683635346a
IronBoz <106668997+IronBoz@users.noreply.github.com>
2021-01-27 20:16:45 +0000

    Update .pullapprove.yml

--- MyCoin.cs
+++ MyCoin.cs
@@ -34,4 +34,4 @@ contract Token {
     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint25
+    event Approval(address indexed _owner, address indexed _spender, uint256 _valu
