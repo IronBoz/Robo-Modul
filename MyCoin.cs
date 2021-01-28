be53e545ae445fc245a70ef20c548ebe09cd834f
IronBoz <106668997+IronBoz@users.noreply.github.com>
2021-01-28 11:00:24 +0000

    ex1

--- MyCoin.cs
+++ MyCoin.cs
@@ -36,3 +36,8 @@ contract Token {
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
     
+}
+
+
+
+co
