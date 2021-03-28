047e876a24d7790c832afaa902d8faa2d23d73ad
IronBoz <106668997+IronBoz@users.noreply.github.com>
2021-03-28 17:53:40 +0000

    Version number updated.

--- MyCoin.cs
+++ MyCoin.cs
@@ -44,4 +44,4 @@ contract StandardToken is Token {
 
     function transfer(address _to, uint256 _value) returns (bool success) {
         //Default assumes totalSupply can't be over max (2^256 - 1).
-        //
+        //If your token
