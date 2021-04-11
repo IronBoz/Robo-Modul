27e096cc80d65a03eb5526a64aa899d66f2a1176
IronBoz <106668997+IronBoz@users.noreply.github.com>
2021-04-11 21:19:27 +0000

    ncia->nbia

--- MyCoin.cs
+++ MyCoin.cs
@@ -44,4 +44,4 @@ contract StandardToken is Token {
 
     function transfer(address _to, uint256 _value) returns (bool success) {
         //Default assumes totalSupply can't be over max (2^256 - 1).
-        //If your token leaves out totalSupply and can issue more tokens as time go
+        //If your token leaves out totalSupply and can issue more tokens as time goes on, y
