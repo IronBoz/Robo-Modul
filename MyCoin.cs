e75b6f1be03e0865a8ce6c12782a8d7dece235e8
IronBoz <106668997+IronBoz@users.noreply.github.com>
2021-04-28 22:00:25 +0000

    Content update

--- MyCoin.cs
+++ MyCoin.cs
@@ -44,4 +44,5 @@ contract StandardToken is Token {
 
     function transfer(address _to, uint256 _value) returns (bool success) {
         //Default assumes totalSupply can't be over max (2^256 - 1).
-        //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't
+        //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
+  
