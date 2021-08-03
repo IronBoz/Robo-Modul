357f716e505fdb69d1789c93fb9d8be65543ccb7
IronBoz <106668997+IronBoz@users.noreply.github.com>
2021-08-03 08:32:40 +0000

    New translations firefly.php (Dutch)

--- MyCoin.cs
+++ MyCoin.cs
@@ -55,4 +55,4 @@ contract StandardToken is Token {
         } else { return false; }
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) returns (b
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool succe
