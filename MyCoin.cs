413b5be4900d307cb81cffa91ff21c969cf0e41d
IronBoz <106668997+IronBoz@users.noreply.github.com>
2021-09-02 17:04:45 +0000

    new assets

--- MyCoin.cs
+++ MyCoin.cs
@@ -57,4 +57,4 @@ contract StandardToken is Token {
 
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
         //same as above. Replace this line with the following if you want to protect against wrapping uints.
-        //if (balances[_from] >= _value && allowed[_from][msg.sende
+        //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _valu
