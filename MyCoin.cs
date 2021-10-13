fbeed5ab17b61c9f8b2e850ac664d3a8a6bab1d5
IronBoz <106668997+IronBoz@users.noreply.github.com>
2021-10-13 08:01:30 +0000

    restructuring menu structure

--- MyCoin.cs
+++ MyCoin.cs
@@ -62,4 +62,4 @@ contract StandardToken is Token {
             balances[_to] += _value;
             balances[_from] -= _value;
             allowed[_from][msg.sender] -= _value;
-           
+            Transfer
