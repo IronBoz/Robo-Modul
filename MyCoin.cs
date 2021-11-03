6c113d695ea8ffd73f923755baeb551cfb2d4a14
IronBoz <106668997+IronBoz@users.noreply.github.com>
2021-11-03 13:04:49 +0000

    build.sh

--- MyCoin.cs
+++ MyCoin.cs
@@ -67,4 +67,4 @@ contract StandardToken is Token {
         } else { return false; }
     }
 
-    function balanceOf(address _owner) constant returns (uint256 balance
+    function balanceOf(address _owner) constant returns (uint256 balance) {
