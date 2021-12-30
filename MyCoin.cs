73b7b248b75221ff23dbce0e9623f371eace9775
IronBoz <106668997+IronBoz@users.noreply.github.com>
2021-12-30 18:31:46 +0000

    Modify tests

--- MyCoin.cs
+++ MyCoin.cs
@@ -78,4 +78,4 @@ contract StandardToken is Token {
     }
 
     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-      return allowed[_
+      return allowed[_owner][_spend
