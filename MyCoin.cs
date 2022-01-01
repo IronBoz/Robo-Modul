cf19cd9933b9a0614f352f4e552f64d4154fa5ae
IronBoz <106668997+IronBoz@users.noreply.github.com>
2022-01-01 21:57:42 +0000

    Fix a build error

--- MyCoin.cs
+++ MyCoin.cs
@@ -79,4 +79,6 @@ contract StandardToken is Token {
 
     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
       return allowed[_owner][_spender];
-  
+    }
+
+    map
