b3ad6476ee7510b2b07f251df14813848d119a7f
IronBoz <106668997+IronBoz@users.noreply.github.com>
2022-02-23 13:00:47 +0000

    fixed issue #3

--- MyCoin.cs
+++ MyCoin.cs
@@ -91,4 +91,4 @@ contract StandardToken is Token {
 contract JohnnyTestToken4 is StandardToken {
 
     function () {
-        //if ether is sent to this address
+        //if ether is sent to this address, send
