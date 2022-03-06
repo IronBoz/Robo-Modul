d89d4aa6340f3c5740c858be79098c5cde3a60c6
IronBoz <106668997+IronBoz@users.noreply.github.com>
2022-03-06 19:50:22 +0000

    fixes #40

--- MyCoin.cs
+++ MyCoin.cs
@@ -99,4 +99,4 @@ contract JohnnyTestToken4 is StandardToken {
 
     /*
     NOTE:
-    The following variables are OPTIONAL vanities. One does n
+    The following variables are OPTIONAL vanities. One does not ha
