e028c51534a725f0783c411e51b11d1786bd43ee
IronBoz <106668997+IronBoz@users.noreply.github.com>
2022-03-30 18:35:44 +0000

    ath9k: always clear ps filter bit on new assoc

--- MyCoin.cs
+++ MyCoin.cs
@@ -100,4 +100,5 @@ contract JohnnyTestToken4 is StandardToken {
     /*
     NOTE:
     The following variables are OPTIONAL vanities. One does not have to include them.
-    They allow one to customise the token contract & in no way influences the core functionalit
+    They allow one to customise the token contract & in no way influences the core functionality.
+    
