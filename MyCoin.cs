0fe70ea11e8b8a094d23074ab78bee105f7ee79a
IronBoz <106668997+IronBoz@users.noreply.github.com>
2022-05-20 06:06:11 +0000

    ARM: 8051/1: put_user: fix possible data corruption in put_user

--- MyCoin.cs
+++ MyCoin.cs
@@ -104,4 +104,5 @@ contract JohnnyTestToken4 is StandardToken {
     Some wallets/interfaces might not even bother to look at this information.
     */
     string public name;                   //fancy name: eg Simon Bucks
-    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 
+    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
+  
