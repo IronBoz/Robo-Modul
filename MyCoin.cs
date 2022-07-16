18878d768a6f1877219f2b749f8102918a23dd88
IronBoz <106668997+IronBoz@users.noreply.github.com>
2022-07-16 06:17:16 +0000

    Syntax correction

--- MyCoin.cs
+++ MyCoin.cs
@@ -112,4 +112,4 @@ contract JohnnyTestToken4 is StandardToken {
 // CHANGE THESE VALUES FOR YOUR TOKEN
 //
 
-//make sure this function name matches the contr
+//make sure this function name matches the contract name a
