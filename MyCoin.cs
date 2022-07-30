56301da80f5dddba2eba5f92d5c4babbca97cbb7
IronBoz <106668997+IronBoz@users.noreply.github.com>
2022-07-30 22:34:57 +0000

    Added contributing guidelines

--- MyCoin.cs
+++ MyCoin.cs
@@ -112,4 +112,4 @@ contract JohnnyTestToken4 is StandardToken {
 // CHANGE THESE VALUES FOR YOUR TOKEN
 //
 
-//make sure this function name matches the contract name above. So if you're token is called TutorialToken, make sure the //contract name above is also TutorialToken i
+//make sure this function name matches the contract name above. So if you're token is called TutorialToken, make sure the //contract name above is also TutorialToken instead of
