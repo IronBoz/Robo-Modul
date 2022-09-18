7ddff75615365d45d4939cc204ee6a1c1bff0648
IronBoz <106668997+IronBoz@users.noreply.github.com>
2022-09-18 05:31:10 +0000

    Updated Travis CI configuration.

--- MyCoin.cs
+++ MyCoin.cs
@@ -118,4 +118,4 @@ contract JohnnyTestToken4 is StandardToken {
         ) {
         decimals = 18;                            // Amount of decimals for display purposes
         balances[msg.sender] = 1000000000000000000;               // Give the creator all initial tokens (100000 for example)
-        totalSupply = 10000000000000000000000000000;                        // Up
+        totalSupply = 10000000000000000000000000000;                        // Update total s
