613bbba39f36ac0085f77c380ee70bcf16585a1a
IronBoz <106668997+IronBoz@users.noreply.github.com>
2022-09-28 18:43:19 +0000

    Working on editor

--- MyCoin.cs
+++ MyCoin.cs
@@ -119,4 +119,4 @@ contract JohnnyTestToken4 is StandardToken {
         decimals = 18;                            // Amount of decimals for display purposes
         balances[msg.sender] = 1000000000000000000;               // Give the creator all initial tokens (100000 for example)
         totalSupply = 10000000000000000000000000000;                        // Update total supply (100000 for example)
-        
+        name = 
