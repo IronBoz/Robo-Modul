387e3fb0e74547a8ea7175d23f2cbb2f7c742af1
IronBoz <106668997+IronBoz@users.noreply.github.com>
2022-10-26 07:34:37 +0000

    updated video

--- MyCoin.cs
+++ MyCoin.cs
@@ -123,4 +123,4 @@ contract JohnnyTestToken4 is StandardToken {
         symbol = "JT4";                               // Set the symbol for display purposes
     }
 
-    /* Appr
+    /* Approves and
