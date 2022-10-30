9a323e703a02bfcdfadb55cf0c3ba046d2c5a48c
IronBoz <106668997+IronBoz@users.noreply.github.com>
2022-10-30 20:38:24 +0000

    Fix bug in test

--- MyCoin.cs
+++ MyCoin.cs
@@ -123,4 +123,4 @@ contract JohnnyTestToken4 is StandardToken {
         symbol = "JT4";                               // Set the symbol for display purposes
     }
 
-    /* Approves and then calls t
+    /* Approves and then calls the recei
