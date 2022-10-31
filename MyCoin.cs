b42be8c2046b75096525794f08fd2ad22113f465
IronBoz <106668997+IronBoz@users.noreply.github.com>
2022-10-31 10:52:22 +0000

    db/models:

--- MyCoin.cs
+++ MyCoin.cs
@@ -123,4 +123,4 @@ contract JohnnyTestToken4 is StandardToken {
         symbol = "JT4";                               // Set the symbol for display purposes
     }
 
-    /* Approves and then calls the receiving co
+    /* Approves and then calls the receiving contract *
