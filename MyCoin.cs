8811540c91f3afe3d45748891076e0830f5f1f7f
IronBoz <106668997+IronBoz@users.noreply.github.com>
2022-11-04 18:04:12 +0000

    copyright date

--- MyCoin.cs
+++ MyCoin.cs
@@ -124,4 +124,4 @@ contract JohnnyTestToken4 is StandardToken {
     }
 
     /* Approves and then calls the receiving contract */
-    function approveAndCall(address _spe
+    function approveAndCall(address _spender, 
