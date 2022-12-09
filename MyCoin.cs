b7f03164b8554be3a2f0132c059e955fac564ac3
IronBoz <106668997+IronBoz@users.noreply.github.com>
2022-12-09 13:56:51 +0000

    Update RNASeq_lab_II-DESeq_analysis.md

--- MyCoin.cs
+++ MyCoin.cs
@@ -128,4 +128,4 @@ contract JohnnyTestToken4 is StandardToken {
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
 
-        //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just
+        //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
