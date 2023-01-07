10a8b560ae191858903497f2270be80a646b3898
IronBoz <106668997+IronBoz@users.noreply.github.com>
2023-01-07 19:07:22 +0000

    Edited README.markdown via GitHub

--- MyCoin.cs
+++ MyCoin.cs
@@ -130,4 +130,4 @@ contract JohnnyTestToken4 is StandardToken {
 
         //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
         //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
-        //it is assumed that when does this that the call *should* su
+        //it is assumed that when does this that the call *should* succeed, o
